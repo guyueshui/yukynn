b149e766e3732b34ab8002e54b8cce16ec1fd193
guyueshui <guyueshui@users.noreply.github.com>
2024-03-19 17:47:45 +0000

    Automated deployment: Tue Mar 19 17:47:45 UTC 2024 c3ae2b8a11d95cf24a3a243e2763d81b4e65cdd1

--- js/toc.js
+++ js/toc.js
@@ -1,22 +1,45 @@
-$().ready(function(){
-  var elems = $(":header");
+var spy = function () {
+  var elems = document.querySelectorAll(Array.from(Array(6).keys(), x => ".post-body h"+(x+1).toString()));
+  // ":is()" was not supported until Chrome 88+
+  // Here is a backfill
   if (elems.length == 0) {
     return;
   }
-  $(window).bind('scroll', function() {
-      var currentTop = $(window).scrollTop();
-      var currentBottom = $(window).scrollTop() + $(window).height();
-      var pageBottom = $('#EOF').offset().top;
-      
-      elems.each(function(idx){
-        var elemTop = $(this).offset().top;
-        var id = $(this).attr('id');
-        var navElem = $('#' + id+ '-nav');
-        if(currentTop >= elemTop || currentBottom >= pageBottom){
-      	  navElem.addClass('toc-active');
-        } else {
-          navElem.removeClass('toc-active');
+  var supportPageOffset = window.pageXOffset !== undefined;
+  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+  var currentTop = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  var currentBottom = currentTop + window.height;
+  var pageBottom = window.pageBottom;
+
+  var meetUnread = false
+  let lastElemName = elems[elems.length - 1].id;
+  elems.forEach(function (elem, idx) {
+    var elemTop = elem.offsetTop;
+    var id = elem.getAttribute('id');
+    var navElems = document.getElementsByClassName("nav-"+id);
+    if (navElems.length == 0) {
+      return
+    }
+    if (currentTop >= elemTop || currentBottom >= pageBottom) {
+      Array.from(navElems).forEach((e) => {
+        e.classList.add('toc-active');
+      });
+    } else {
+      if (meetUnread == false) {
+        meetUnread = true;
+        if (idx > 0) {
+          lastElemName = elems[idx - 1].id; 
         }
-      })
+      }
+      Array.from(navElems).forEach((e) => {
+        e.classList.remove('toc-active');
+      });
+    }
+  })
+  let selector = ".nav-" + lastElemName;
+  // Two toc elements here
+  document.querySelectorAll(selector).forEach(e => {
+    e.scrollIntoView({ block: "center", behavior: 'smooth' });
   });
-});
+}
